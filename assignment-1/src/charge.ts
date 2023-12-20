3bcee15757e48d46a41dbd9eafb0ff2c4dcab5d3
fumiyadoi <fdoi1996@gmail.com>
2023-12-20 16:11:50 +0900

    refactor: 課題用リファクタ
    以下リファクタしました
    - Paymentのtypeをstring -> union型に変更
    - 支払額計算用関数を作成しました
    - お釣り計算用関数を作成しました

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -9,42 +9,56 @@ export type Receipt = {
 };
 
 export type Payment = {
-  type: string;
+  type: 'CASH' | 'COUPON';
   percentage?: number;
   amount?: number;
 };
 
-export function charge(invoice: Invoice, payments: Payment[]) {
-  const total = invoice.total;
-  let deposit = 0;
-
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
+// 支払額計算用の関数
+export function calculateDeposit(total: number, payments: Payment[]): number {
+  // 現金とクーポンの合計をそれぞれ計算する
+  const { cashDeposit, couponDeposit } = payments.reduce(
+    (acc, payment) => {
+      if (payment.type === 'CASH') {
+        acc.cashDeposit += payment.amount || 0;
+      } else if (payment.type === 'COUPON') {
         if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
+          acc.couponDeposit += Math.floor(total * (payment.percentage / 100));
         } else {
-          deposit += payment.amount || 0;
+          acc.couponDeposit += payment.amount || 0;
         }
-      } else {
-        if (deposit >= total) {
-          throw new Error('OverCharge');
-        }
-        deposit += payment.amount || 0;
       }
-    });
-  if (total > deposit) {
-    throw new Error('Shortage');
-  }
+      return acc;
+    },
+    { cashDeposit: 0, couponDeposit: 0 },
+  );
 
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
-  }
-  if (isCoupon) return { total, deposit, change: 0 };
-  return { total: total, deposit: deposit, change: deposit - total };
+  // クーポンの合計が合計金額を超えている、かつ現金支払いがある場合はエラーを起こす
+  if (couponDeposit >= total && cashDeposit) throw new Error('OverCharge');
+
+  const deposit = cashDeposit + couponDeposit;
+
+  // 支払い額が足りない場合はエラーを起こす
+  if (total > deposit) throw new Error('Shortage');
+
+  return deposit;
+}
+
+// お釣り計算用の関数
+export function calculateChange(total: number, deposit: number, payments: Payment[]): number {
+  // クーポンのみで支払っている場合はお釣りは0
+  const isCouponOnly = !payments.some((payment) => payment.type === 'CASH');
+
+  return isCouponOnly ? 0 : deposit - total;
+}
+
+// 支払い額を計算してレシートを返す
+export function charge(invoice: Invoice, payments: Payment[]): Receipt {
+  const total = invoice.total;
+
+  const deposit = calculateDeposit(total, payments);
+
+  const change = calculateChange(total, deposit, payments);
+
+  return { total, deposit, change } as Receipt;
 }
